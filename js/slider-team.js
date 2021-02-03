10eab265922ea2c976f1110656d8007bd0f9dfc7
MooMlce <54530553+MooMlce@users.noreply.github.com>
2021-02-03 12:29:33 +0300

    Add files via upload

--- js/slider-team.js
+++ js/slider-team.js
@@ -1,22 +1,29 @@
 $(document).ready(function(){
 
-  $('.reviews__items').slick({
+  $('.team-company__items').slick({
     arrows:false,
     dots:true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 3,
+          slidesToScroll: 3,
           infinite: true,
           dots: true
         }
       },
       {
-        breakpoint: 725,
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 560,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
